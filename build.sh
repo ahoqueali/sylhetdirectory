b341c82825584ebcaf78c376800b8f5082d60a37
Abdulhoque Ali <abdulhoque@Abdulhoques-MacBook-Pro.local>
2023-07-09 18:04:38 +0100

    update scripts

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-export JAVA_HOME=$(/usr/libexec/java_home -v14)
+export JAVA_HOME=$(/usr/libexec/java_home -v20)
 #export deleteListings=true
 export rootDir=docs
 ./compile.sh
-$JAVA_HOME/bin/java --enable-preview DirectoryBuilder dataset-2.tsv
-# $JAVA_HOME/bin/java --enable-preview DirectoryBuilder dataset-1.tsv dataset-2.tsv dataset-20200718.tsv dataset-20201227.tsv
+$JAVA_HOME/bin/java --enable-preview DirectoryBuilder /Users/abdulhoque/Downloads/sylhetdirectory.tsv
