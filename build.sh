973ac0f587e6db7c6e4eaa33d3fb0e9cdd123511
Abdulhoque Ali <abdulhoque@Abdulhoques-MacBook-Pro.local>
2023-09-05 06:38:20 +0100

    update wedding story listing

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ export JAVA_HOME=$(/usr/libexec/java_home -v20)
 export rootDir=docs
 rm -rf docs/categories
 ./compile.sh
-$JAVA_HOME/bin/java --enable-preview DirectoryBuilder dataset-2.tsv
+$JAVA_HOME/bin/java --enable-preview DirectoryBuilder /Users/abdulhoque/Downloads/sylhetdirectory-20230905.tsv
+#$JAVA_HOME/bin/java --enable-preview DirectoryBuilder dataset-2.tsv
